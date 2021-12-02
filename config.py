16774497726d5db55fed4c6ea5ef23e5ef175398
CI314X <dkonstantinov0@gmail.com>
2021-12-02 14:24:43 +0300

    add config

---
+++ config.py
@@ -0,0 +1,17 @@
+# email server (particularly, mail.ru)
+MAIL_SERVER = 'smtp.mail.ru'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+SECRET_KEY=''
+WTF_CSRF_SECRET_KEY=''
+
+# administrator list
+ADMINS = ['']
+
+# vkontakte
+access_token = ['']
+
+# facebook
